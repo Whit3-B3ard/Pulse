4f84b518f901486bc118735ccdb6e885866a0f49
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2023-11-29 09:36:23 +0100

    newsletter

--- Pulse/src/pages/newsLetter.jsx
+++ Pulse/src/pages/newsLetter.jsx
@@ -1,7 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function NewsLetter() {
+const NewsLetter = () => {
+  const readSubscriptions = () => {
+    const items = localStorage.getItem("subscriptions");
+    return JSON.parse(items) || [];
+  };
+
+  const [email, setEmail] = useState("");
+  const [subscribe, setSubscribe] = useState(false);
+  const [subscriptions, setSubscriptions] = useState(readSubscriptions());
+
+  const handleSubscribe = () => {
+    const newSubscription = { email, subscribe };
+    setSubscriptions([...subscriptions, newSubscription]);
+    setEmail("");
+    setSubscribe(false);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("subscriptions", JSON.stringify(subscriptions));
+  }, [subscriptions]);
+
   return (
-    <div>newsLetter</div>
-  )
-}
+    <div className="flex w-screen h-screen flex-col bg-gray-950">
+      <div className="flex w-screen h-screen flex-col bg-black">
+      <div className="w-full h-2/4 bg-black flex">
+    <div className='flex justify-center flex-1'>
+        <img src="https://img.freepik.com/free-vector/silhouettes-people-dancing-glittery-background_1048-17649.jpg" className="h-full" alt="" />
+    </div>
+    <div className='flex justify-end flex-1 relative'>
+        <img src="https://c4.wallpaperflare.com/wallpaper/692/778/500/heartbeat-minimalism-lines-pulse-wallpaper-preview.jpg" className="h-full" alt="" />
+        <div className="absolute inset-0 bg-black opacity-40"></div>
+    </div>
+</div>
+
+</div>
+
+    <div className='flex absolute z-10 inset-0 items-center'>
+      <div className="max-w-md mx-auto bg-white rounded-lg p-6 shadow-xl h-96 pt-12 ">
+        
+        <h2 className="text-2xl font-semibold mb-4">Subscribe to Our Newsletter</h2>
+        <p className="text-gray-600 mb-6">Stay updated with our latest Songs and Albums.</p>
+
+        <form action="#" method="POST" className="space-y-4">
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-sm font-semibold text-gray-600 mb-1">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="you@example.com"
+              className="py-2 px-3 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="subscribe"
+              name="subscribe"
+              className="text-blue-500 rounded-full p-2 border border-blue-500"
+              checked={subscribe}
+              onChange={() => setSubscribe(!subscribe)}
+            />
+            <label htmlFor="subscribe" className="ml-2 text-gray-600">Subscribe to weekly newsletter</label>
+          </div>
+
+          <button type="button" onClick={handleSubscribe} className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition duration-300">Subscribe</button>
+        </form>
+      </div>
+      </div>
+      <div className="text-center text-white py-6 bg-black">
+        <p className="text-2xl font-semibold">Explore the Pulse of Music.</p>
+        <p className="text-gray-300">Discover a world of Tones, Notes, and Musical inspiration on our platform. Join our community of music lovers and stay connected.</p>
+      </div>
+    </div>
+  );
+};
+
+export default NewsLetter;
