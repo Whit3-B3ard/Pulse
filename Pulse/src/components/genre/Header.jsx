7c74067ceaa8aa4f8ff0515dda8a5e569e975ef3
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2023-11-30 12:10:49 +0100

    adss: margon top is added

--- Pulse/src/components/genre/Header.jsx
+++ Pulse/src/components/genre/Header.jsx
@@ -1,6 +1,7 @@
 
 export const Header = (props) => {
   return (
-    <div style={{textAlign:'justify',color:'aquamarine', fontSize:'2em'}}>{props.name}</div>
+    <div style={{textAlign:'justify',color:'aquamarine', fontSize:'2em', marginTop: '80px'}}>{props.name}</div>
+    
   )
 }
